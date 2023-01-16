e9f8be32bad703bdf3d23ed5af06af95ebdd8666
christiandarvs <christiandarvin4@gmail.com>
2023-01-16 22:30:09 +0800

    dribble_clone_2

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dribble_clone_2/on_boarding.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: OnBoard());
+  }
+}
---
+++ lib/on_boarding.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const heading = 'All done!';
+const message =
+    'Congrats = you\'re all done. You can see the status of your card order from your account.';
+const button1 = 'Add money';
+const button2 = 'Go to my card';
+
+class OnBoard extends StatelessWidget {
+  const OnBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column()),
+    );
+  }
+}
